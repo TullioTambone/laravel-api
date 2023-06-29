bf63d32a869af7d4c25e9014a57890b3291d35bc
TullioTambone <tulliotambone97@gmail.com>
2023-06-29 12:30:13 +0200

    creazione show() in api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,4 +16,20 @@ class ProjectController extends Controller
 		   'projects' => $projects
 		]);
 	}
+
+	public function show($slug){
+		$project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+		if ($project){
+			return response()->json([
+				'success' => true,
+				'project' => $project
+			]);
+		} else {
+			return response()->json([
+				'success' => false,
+				'error' => 'non ci sono posts'
+			]);
+		}
+	}
 }
